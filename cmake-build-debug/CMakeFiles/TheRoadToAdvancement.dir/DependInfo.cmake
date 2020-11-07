c2b004eab24394b135e2dc7e09eeb53bfe8093a6
LAICHAO20188 <619642846@qq.com>
2020-11-07 17:32:02 +0800

    modify cmakelists

--- cmake-build-debug/CMakeFiles/TheRoadToAdvancement.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/TheRoadToAdvancement.dir/DependInfo.cmake
@@ -16,8 +16,8 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
-  "../include/DataStructure"
+  ".././include"
+  ".././include/DataStructure"
   )
 
 # Targets to which this target links.
