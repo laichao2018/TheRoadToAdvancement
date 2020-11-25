92cd38cfc0126a45f115b138f47fcebaf2c7f88e
LAICHAO20188 <619642846@qq.com>
2020-11-25 22:20:56 +0800

    20201125 - DailyCoding

--- include/DailyCoding.h
+++ include/DailyCoding.h
@@ -82,6 +82,7 @@ public:
     static bool isAnagram(string s, string t);              // 242. 有效的字母异位词
     static int findMinArrowShots(vector<vector<int>>& points);          // 452. 用最少数量的箭引爆气球
     static int countNodes(TreeNode* root);          // 222. 完全二叉树的节点个数
+    static string sortString(string s);             // 1370. 上升下降字符串
 };
 
 #endif //THEROADTOADVANCEMENT_DAILYCODING_H
--- src/DailyCoding.cpp
+++ src/DailyCoding.cpp
@@ -1372,6 +1372,30 @@ int DailyCoding::countNodes(TreeNode *root) {
     return low;
 }
 
+string DailyCoding::sortString(string s) {
+    vector<int> num(26);
+    for (char &ch : s) {
+        num[ch - 'a']++;
+    }
+
+    string ret;
+    while (ret.length() < s.length()) {
+        for (int i = 0; i < 26; i++) {
+            if (num[i]) {
+                ret.push_back(i + 'a');
+                num[i]--;
+            }
+        }
+        for (int i = 25; i >= 0; i--) {
+            if (num[i]) {
+                ret.push_back(i + 'a');
+                num[i]--;
+            }
+        }
+    }
+    return ret;
+}
+
 
 
 
