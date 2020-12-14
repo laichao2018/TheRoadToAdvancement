a4248c3bf58ee004a930245f54ccead1c9e2a5b6
LAICHAO2018 <619642846@qq.com>
2020-12-14 21:39:17 +0800

    20201214 - DailyCoding

--- src/DailyCoding.cpp
+++ src/DailyCoding.cpp
@@ -1763,13 +1763,13 @@ bool DailyCoding::containsDuplicate(vector<int> &nums) {
 
 vector<vector<string>> DailyCoding::groupAnagrams(vector<string> &strs) {
     unordered_map<string, vector<string>> mp;
-    for (string& str: strs) {
+    for (string &str:strs) {
         string key = str;
         sort(key.begin(), key.end());
         mp[key].emplace_back(str);
     }
     vector<vector<string>> ans;
-    for (auto it = mp.begin(); it != mp.end(); ++it) {
+    for (auto it = mp.begin(); it != mp.end(); it++) {
         ans.emplace_back(it->second);
     }
     return ans;
