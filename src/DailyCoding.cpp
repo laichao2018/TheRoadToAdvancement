9cdc05e170d46e7981370dccaefc88f83e6d8884
laichao <619642846@qq.com>
2021-01-13 22:10:38 +0800

    20210113 - DailyCoding

--- src/DailyCoding.cpp
+++ src/DailyCoding.cpp
@@ -317,6 +317,32 @@ bool isPrimer(int n) {
     return true;
 }
 
+vector<int> topSort(vector<int> &deg, vector<vector<int>> &graph, vector<int> &items) {
+    queue<int> q;
+    for (auto item:items)
+        if (deg[item] == 0) q.push(item);
+    vector<int> res;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        res.emplace_back(u);
+        for (auto &v:graph[u])
+            if (--deg[v] == 0) q.push(v);
+    }
+    return res.size() == items.size() ? res : vector<int>{};
+}
+
+int find_func(vector<int> &parent, int index) { // findRedundantConnection - 并查集
+    if (parent[index] != index) {
+        parent[index] = find_func(parent, parent[index]);
+    }
+    return parent[index];
+}
+
+void union_func(vector<int> &parent, int index01, int index02) {
+    parent[find_func(parent, index01)] = find_func(parent, index02);
+}
+
 ///// ================================== HELP CLASS ==================================
 class Djset {
 public:
@@ -2106,4 +2132,82 @@ string DailyCoding::smallestStringWithSwaps(string s, vector<vector<int>> &pairs
     s = "";
     for (const auto &e : rs) s += e;
     return s;
+}
+
+vector<int> DailyCoding::sortItems(int n, int m, vector<int> &group, vector<vector<int>> &beforeItems) {
+    vector<vector<int>> groupItem(n + m);
+
+    // 组间和组内依赖图
+    vector<vector<int>> groupGraph(n + m);
+    vector<vector<int>> itemGraph(n);
+
+    // 组间和组内入度数组
+    vector<int> groupDegree(n + m, 0);
+    vector<int> itemDegree(n, 0);
+
+    vector<int> id;
+    for (int i = 0; i < n + m; ++i) {
+        id.emplace_back(i);
+    }
+
+    int leftId = m;
+    // 给未分配的 item 分配一个 groupId
+    for (int i = 0; i < n; ++i) {
+        if (group[i] == -1) {
+            group[i] = leftId;
+            leftId += 1;
+        }
+        groupItem[group[i]].emplace_back(i);
+    }
+    // 依赖关系建图
+    for (int i = 0; i < n; ++i) {
+        int curGroupId = group[i];
+        for (auto &item: beforeItems[i]) {
+            int beforeGroupId = group[item];
+            if (beforeGroupId == curGroupId) {
+                itemDegree[i] += 1;
+                itemGraph[item].emplace_back(i);
+            } else {
+                groupDegree[curGroupId] += 1;
+                groupGraph[beforeGroupId].emplace_back(curGroupId);
+            }
+        }
+    }
+
+    // 组间拓扑关系排序
+    vector<int> groupTopSort = topSort(groupDegree, groupGraph, id);
+    if (groupTopSort.size() == 0) {
+        return vector<int>{};
+    }
+    vector<int> ans;
+    // 组内拓扑关系排序
+    for (auto &curGroupId: groupTopSort) {
+        int size = groupItem[curGroupId].size();
+        if (size == 0) {
+            continue;
+        }
+        vector<int> res = topSort(itemDegree, itemGraph, groupItem[curGroupId]);
+        if (res.size() == 0) {
+            return vector<int>{};
+        }
+        for (auto &item: res) {
+            ans.emplace_back(item);
+        }
+    }
+    return ans;
+}
+
+vector<int> DailyCoding::findRedundantConnection(vector<vector<int>> &edges) {
+    int nodeCounts = edges.size();
+    vector<int> parent(nodeCounts + 1);
+    for (int i = 1; i < nodeCounts + 1; i++) {
+        parent[i] = i;
+    }
+    for (auto &edge:edges) {
+        int node01 = edge[0], node02 = edge[1];
+        if (find_func(parent, node01) != find_func(parent, node02)) {
+            union_func(parent, node01, node02);
+        } else return edge;
+    }
+    return {};
 }
