c94187d22144cea0c02fe81f7334da23e5b1cf3e
laichao <619642846@qq.com>
2021-01-23 22:18:36 +0800

    更新AcWing

--- include/AcWingSolutions.h
+++ include/AcWingSolutions.h
@@ -6,15 +6,26 @@
 #define THEROADTOADVANCEMENT_ACWINGSOLUTIONS_H
 
 #include "./__OSJudge.h"
+#include "./DataStructure/List.h"
 #include <vector>
 
 using namespace std;
 
 // 算法基础课相关代码模板
-class BaseTemplate{
+class BaseTemplate {
 public:
-    static void quick_sort(vector<int>&nums);       // 785. 快速排序
-    static void merge_sort(vector<int>&nums);       // 787. 归并排序
+    static void quick_sort(vector<int> &nums);       // 785. 快速排序
+    static void merge_sort(vector<int> &nums);       // 787. 归并排序
+};
+
+class AcStar {
+public:
+    static int getNumberOfK(vector<int> &nums, int k);      // 67. 数字在排序数组中出现的次数
+    static int getMissingNumber(vector<int> &nums);         // 68. 0到n-1中缺失的数字
+    static void reOrderArray(vector<int> &array);           // 32. 调整数组顺序使奇数位于偶数前面
+    static vector<int> printListReversingly(ListNode *head);    // 17. 从尾到头打印链表
+    static vector<int> getLeastNumbers_Solution(vector<int> input, int k);  // 53. 最小的k个数
+    static vector<int> findNumbersWithSum(vector<int> &nums, int target);   // 75. 和为S的两个数字
 };
 
 
--- src/AcWingSolutions.cpp
+++ src/AcWingSolutions.cpp
@@ -4,6 +4,11 @@
 
 #include "AcWingSolutions.h"
 #include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <unordered_set>
 
 using namespace std;
 
@@ -66,3 +71,102 @@ void BaseTemplate::quick_sort(vector<int> &nums) {
 void BaseTemplate::merge_sort(vector<int> &nums) {
     HelpFunc_merge_sort(nums, 0, nums.size() - 1);
 }
+
+int AcStar::getNumberOfK(vector<int> &nums, int k) {
+    unordered_map<int, int> _map;
+    for (int i:nums) {
+        _map[i]++;
+    }
+    return _map[k];
+}
+
+int AcStar::getMissingNumber(vector<int> &nums) {
+    int curr = 0;
+    for (int i = 0; i < nums.size(); i++) curr += nums[i];
+    int true_sum = 0;
+    for (int i = 0; i < nums.size() + 1; i++) true_sum += i;
+    return true_sum - curr;
+}
+
+void AcStar::reOrderArray(vector<int> &array) {
+    int l = 0, r = array.size() - 1;
+    while (l < r) {
+        while (l < r && array[l] % 2 == 1) l++;
+        while (l < r && array[r] % 2 == 0) r--;
+        if (l < r) swap(array[l], array[r]);
+    }
+}
+
+vector<int> AcStar::printListReversingly(ListNode *head) {
+    vector<int> res;
+    while (head != nullptr) {
+        res.push_back(head->val);
+        head = head->next;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+vector<int> AcStar::getLeastNumbers_Solution(vector<int> input, int k) {
+    priority_queue<int, vector<int>, less<int>> q;
+    for (int i = 0; i < input.size(); i++) {
+        q.push(input[i]);
+        if (q.size() > k) q.pop();
+    }
+    vector<int> res;
+    while (!q.empty()) {
+        res.push_back(q.top());
+        q.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+vector<int> AcStar::findNumbersWithSum(vector<int> &nums, int target) {
+    unordered_set<int> hash;
+    for (int i = 0; i < nums.size(); i++) {
+        if (hash.count(target - nums[i])) return {target - nums[i], nums[i]};
+        else hash.insert(nums[i]);
+    }
+    return {};
+}
+
+// _________________________________OTHER CLASS_________________________________
+// *** 20. 用两个栈实现队列 ***
+class MyQueue {
+public:
+    MyQueue() = default;
+
+    void push(int x) {
+        stk.push(x);
+    }
+
+    int pop() {
+        copy(stk, cache);
+        int res = cache.top();
+        cache.pop();
+        copy(cache, stk);
+        return res;
+    }
+
+    int peek() {
+        copy(stk, cache);
+        int res = cache.top();
+        copy(cache, stk);
+        return res;
+    }
+
+    bool empty() {
+        return stk.empty();
+    }
+
+private:
+    stack<int> stk, cache;
+
+    void copy(stack<int> &a, stack<int> &b) {
+        while (a.size()) {
+            b.push(a.top());
+            a.pop();
+        }
+    }
+};
