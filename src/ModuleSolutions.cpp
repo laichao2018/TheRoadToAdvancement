b4414958228ecde8b4af9542c1f040fccf4e057c
LAICHAO20188 <619642846@qq.com>
2021-03-22 16:39:24 +0800

    20210322 - DailyCoding

--- src/ModuleSolutions.cpp
+++ src/ModuleSolutions.cpp
@@ -2155,7 +2155,7 @@ int MeduimSolutions::maximumUniqueSubarray(vector<int> &nums) {
 
 double MeduimSolutions::myPow(double x, int n) {
     long long N = n;
-    return N >= 0 ? quickMul(x, N) : quickMul(x, -N);
+    return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
 }
 
 // 706. 设计哈希映射
