794ce3a647d324ec58ea40d9eb13d0f81a1ea2c1
LAICHAO20188 <619642846@qq.com>
2021-04-02 20:31:38 +0800

    add hash class //set && map

--- src/HashSet.hpp
+++ src/HashSet.hpp
@@ -3,48 +3,86 @@
 //
 // 哈希函数
 
-#include <list>
 #include <vector>
 
-// 设计哈希集合
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                            HASH_SET                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// 设计哈希映射
+// 采用单链表法
 class MyHashSet {
+private:
+    // 在类中定义单链表结构
+    struct Node {
+        int val;
+        Node *next;
+
+        Node(int _v) : val(_v), next(nullptr) {}
+    };
+
 public:
     /** Initialize your data structure here. */
-    MyHashSet() : data(base) {}
-
-    void add(int key) {
-        int index = hash(key);
-        for (auto it = data[index].begin(); it != data[index].end(); it++) {
-            if (*it == key) return;
-        }
-        data[index].push_back(key);
+    MyHashSet() {
+        bucket = vector<Node *>(hashRange); // 初始化bucket，一定要加初始大小，否则会面的函数中会访问出错
     }
 
+    /** value will always be non-negative. */
+    void add(int key) {
+        int index = hash(key);
+        if (bucket[index] == nullptr) {
+            bucket[index] = new Node(key);  // 没有数据，新增，返回
+            return;
+        }
+        Node *tmp = bucket[index];
+        if (tmp->val == key) return;
+        while (tmp->next != nullptr) {
+            if (tmp->next->val == key) return;
+            tmp = tmp->next;
+        }
+        tmp->next = new Node(key);
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
         int index = hash(key);
-        for (auto it = data[index].begin(); it != data[index].end(); it++) {
-            if (*it == key) {
-                data[index].erase(it);
-                return;
+        if (bucket[index] == nullptr) return;
+        Node *tmp = bucket[index];
+        if (tmp->val == key) {
+            bucket[index] = tmp->next;
+            delete tmp;
+        } else {
+            Node *next = tmp->next;
+            while (next != nullptr) {
+                if (next->val == key) {
+                    tmp->next = next->next;
+                    delete next;
+                    return;
+                }
+                tmp = next;
+                next = next->next;
             }
         }
     }
 
-    /** Returns true if this set contains the specified element */
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     bool contains(int key) {
         int index = hash(key);
-        for (auto it = data[index].begin(); it != data[index].end(); it++) {
-            if (*it == key) return true;
+        Node *tmp = bucket[index];
+        while (tmp != nullptr) {
+            if (tmp->val == key) return true;
+            tmp = tmp->next;
         }
         return false;
     }
 
 private:
-    std::vector<std::list<int>> data;
-    static const int base = 769;    // 为了尽可能避免冲突，应当将base取为一个质数
+    int hashRange = 31;     // 一般为质数，常见的还有769等数字
+    vector<Node *> bucket;
 
-    static int hash(int key) {
-        return key % base;
+    int hash(int key) {
+        return (key % hashRange);
     }
 };
 
@@ -56,27 +94,92 @@ private:
  * bool param_3 = obj->contains(key);
  */
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                            HASH_MAP                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 // 设计哈希映射
+// 采用单链表法
 class MyHashMap {
+private:
+    struct Node {
+        int key;    // 链表中包含了 “键-值”
+        int value;
+        Node *next;
+
+        Node(int _k, int _v) : key(_k), value(_v), next(nullptr) {}
+    };
+
 public:
     /** Initialize your data structure here. */
     MyHashMap() {
-
+        bucket.resize(hashRange);
     }
 
     /** value will always be non-negative. */
     void put(int key, int value) {
-
+        int index = hash(key);
+        if (bucket[index] == nullptr) { // 没有数据，插入
+            bucket[index] = new Node(key, value);
+            return;
+        }
+        Node *tmp = bucket[index];
+        if (tmp->key == key) {  // 修改值
+            bucket[index]->value = value;
+            return;
+        }
+        while (tmp->next != nullptr) {
+            if (tmp->next->key == key) {
+                tmp->next->value = value;
+                return;
+            }
+            tmp = tmp->next;
+        }
+        tmp->next = new Node(key, value);
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) {
-
+        int index = hash(key);
+        if (bucket[index] == nullptr) { // 没有找到该数据
+            return -1;
+        }
+        Node *tmp = bucket[index];
+        if (tmp->key == key) return tmp->value;
+        while (tmp != nullptr) {
+            if (tmp->key == key) return tmp->value;
+            tmp = tmp->next;
+        }
+        return -1;  // 没有找到数据
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) {
+        int index = hash(key);
+        if (bucket[index] == nullptr) return;
+        Node *tmp = bucket[index];
+        if (tmp->key == key) {
+            bucket[index] = tmp->next;
+            delete tmp;
+            return;
+        }
+        Node *next = tmp->next;
+        while (next != nullptr) {
+            if (next->key == key) {
+                tmp->next = next->next;
+                delete next;
+                return;
+            }
+            tmp = next;
+            next = next->next;
+        }
+    }
 
+private:
+    int hashRange = 31;
+    vector<Node *> bucket;
+
+    int hash(int key) {
+        return key % hashRange;
     }
 };
 
