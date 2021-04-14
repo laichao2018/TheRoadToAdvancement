1453286d758b2933c02f57b5d30b4898298a7899
LAICHAO20188 <619642846@qq.com>
2021-04-14 15:08:46 +0800

    20210414 - DailyCoding

--- src/DailyCoding.cpp
+++ src/DailyCoding.cpp
@@ -3139,3 +3139,48 @@ public:
         return data.top();
     }
 };
+
+// 208. 实现 Trie (前缀树)
+class Trie {
+private:
+    vector<Trie *> children;
+    bool isEnd;
+
+    Trie *searchPrefix(string &prefix) {
+        Trie *node = this;
+        for (char c:prefix) {
+            c -= 'a';
+            if (node->children[c] == nullptr) return nullptr;
+            node = node->children[c];
+        }
+        return node;
+    }
+
+public:
+    /** Initialize your data structure here. */
+    Trie() : children(26), isEnd(false) {}
+
+    /** Inserts a word into the trie. */
+    void insert(string &word) {
+        Trie *node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                node->children[ch] = new Trie();
+            }
+            node = node->children[ch];
+        }
+        node->isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node = this->searchPrefix(word);
+        return node != nullptr && node->isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        return this->searchPrefix(prefix) != nullptr;
+    }
+};
