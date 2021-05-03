9b8315cec8737c58d75e7d98e8550dfe210d02a5
laichao2018 <619642846@qq.com>
2021-05-03 18:36:01 +0800

    add 239 - 周赛

--- include/WeekSolutions.h
+++ include/WeekSolutions.h
@@ -12,33 +12,22 @@
 
 using namespace std;
 
+///// ================================== CLASS Declaration ==================================
 class WeekSolutions {
 public:
     //=================================== No.51 - 双周塞 ===================================
     static string replaceDigits(string s);      // 5730. 将所有数字用字符替换
     static int maximumElementAfterDecrementingAndRearranging(vector<int> &arr);     // 5732. 减小和重新排列数组后的最大元素
     static vector<int> closestRoom(vector<vector<int>> &rooms, vector<vector<int>> &queries);       // 5733. 最近的房间
+
+    //=================================== No.239 - 周塞 ===================================
+    static int getMinDistance(vector<int> &nums, int target, int start);            // 1848. 到目标元素的最小距离
+    static bool splitString(string s);          // 1849. 将字符串拆分为递减的连续值
+    static int getMinSwaps(string num, int k);  // 1850. 邻位交换的最小次数
 };
 
 // No.51 - 双周赛
 // 5731. 座位预约管理系统
-class SeatManager {
-private:
-    priority_queue<int, vector<int>, greater<int>> heap;
-public:
-    SeatManager(int n) {
-        for (int i = 1; i <= n; i++) heap.push(i);
-    }
-
-    int reserve() {
-        int minNum = heap.top();
-        heap.pop();
-        return minNum;
-    }
-
-    void unreserve(int seatNumber) {
-        heap.push(seatNumber);
-    }
-};
+class SeatManager;
 
 #endif //THEROADTOADVANCEMENT_WEEKSOLUTIONS_H
--- src/WeekSolutions.cpp
+++ src/WeekSolutions.cpp
@@ -15,6 +15,26 @@ struct Query {
     }
 };
 
+///// =============================== CLASS Definition ===============================
+class SeatManager {
+private:
+    priority_queue<int, vector<int>, greater<int>> heap;
+public:
+    SeatManager(int n) {
+        for (int i = 1; i <= n; i++) heap.push(i);
+    }
+
+    int reserve() {
+        int minNum = heap.top();
+        heap.pop();
+        return minNum;
+    }
+
+    void unreserve(int seatNumber) {
+        heap.push(seatNumber);
+    }
+};
+
 ///// ================================== CLASS FUNC ==================================
 string WeekSolutions::replaceDigits(string s) {
     for (int i = 0; i < s.size(); i += 2) {
@@ -55,6 +75,67 @@ vector<int> WeekSolutions::closestRoom(vector<vector<int>> &rooms, vector<vector
         if (p - *u <= *k - p)res[id] = *u;
         else res[id] = *k;
         if (abs(res[id]) == INF_MAX) res[id] = -1;
+    }
+    return res;
+}
+
+int WeekSolutions::getMinDistance(vector<int> &nums, int target, int start) {
+    int res = INT_MAX;
+    for (int i = 0; i < nums.size(); i++) {
+        if (target == nums[i]) {
+            res = min(res, abs(i - start));
+        }
+    }
+    return res;
+}
+
+bool WeekSolutions::splitString(string s) {
+    // 暴力枚举法， 使用位运算增加速度
+    int n = s.size();
+    for (int i = 1; i < (1 << (n - 1)); i++) {
+        bool flag = true;
+        unsigned long long pre = -1, curr = s[0] - '0';     // 分别存储上一个数和当前数
+        // 从前往后依次枚举
+        for (int j = 0; j < n - 1; j++) {   // 寻找分割位置，规定在数字右边分割，最后一个数字无法分割
+            if ((i >> j) & 1) {     // 如果此位置是1，表示分割线
+                if (pre != -1 && curr != pre - 1) {
+                    flag = false;
+                    break;
+                }
+                pre = x;    // 可以分割
+                x = s[j + 1] - '0';
+            } else {
+                curr = curr * 10 + s[j + 1] - '0';
+            }
+        }
+        if (curr != pre - 1) flag = false;
+        if (flag) return true;
+    }
+    return false;
+}
+
+int WeekSolutions::getMinSwaps(string num, int k) {
+    string s = num;
+    while (k--) next_permutation(s.begin(), s.end());
+    int n = s.size();
+    vector<int> c(n);
+    int cnt[10] = {0};
+    for (int i = 0; i < n; i++) {
+        int x = num[i] - '0';
+        cnt[x]++;
+        int y = cnt[x];
+        for (int j = 0; j < n; j++) {
+            if (s[j] - '0' == x && --y == 0) {
+                c[i] = j;
+                break;
+            }
+        }
+    }
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (c[i] > c[j]) res++;
+        }
     }
     return res;
 }
